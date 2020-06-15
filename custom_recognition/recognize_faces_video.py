88f9d1e0b9f2093964e85245fb9bef26a0e356a8
Simon <trilogicworlds@gmail.com>
2020-06-15 18:57:46 +0200

    support for custom face recognition

--- custom_recognition/recognize_faces_video.py
+++ custom_recognition/recognize_faces_video.py
@@ -1,37 +1,31 @@
-'''
+"""
 Program taken and modified from https://www.pyimagesearch.com/2018/06/18/face-recognition-with-opencv-python-and-deep-learning/
 I do not make any claim over this content, which is property of Adrian Rosebrock.
 Please do not reproduce commercially without permission from the author.
-'''
+"""
 
 # import the necessary packages
-from imutils.video import VideoStream
 import face_recognition
 import argparse
-import imutils
 import pickle
 import time
 import cv2
+import imutils
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument(
     "-e", "--encodings", required=True, help="path to serialized db of facial encodings"
 )
+
 ap.add_argument(
-    "-y",
-    "--display",
-    type=int,
-    default=1,
-    help="whether or not to display output frame to screen",
+    "-u",
+    "--users",
+    type=list,
+    nargs="+",
+    help="list of verified users to brighten screen",
 )
-ap.add_argument(
-    "-d",
-    "--detection-method",
-    type=str,
-    default="cnn",
-    help="face detection model to use: either `hog` or `cnn`",
-)
+
 args = vars(ap.parse_args())
 
 # load the known faces and embeddings
@@ -40,14 +34,15 @@ data = pickle.loads(open(args["encodings"], "rb").read())
 # initialize the video stream and pointer to output video file, then
 # allow the camera sensor to warm up
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-writer = None
 time.sleep(2.0)
 
+verified_users = []
+for i in range(len(args["users"])):
+    verified_users.append(''.join(args["users"][i]))
+    
 # loop over frames from the video file stream
-while True:
+def get_name(frame):
     # grab the frame from the threaded video stream
-    frame = vs.read()
 
     # convert the input frame from BGR to RGB then resize it to have
     # a width of 750px (to speedup processing)
@@ -57,16 +52,15 @@ while True:
     # detect the (x, y)-coordinates of the bounding boxes
     # corresponding to each face in the input frame, then compute
     # the facial embeddings for each face
-    boxes = face_recognition.face_locations(rgb, model=args["detection_method"])
+    boxes = face_recognition.face_locations(rgb, model="hog")
     encodings = face_recognition.face_encodings(rgb, boxes)
     names = []
-
+    name = "Unknown"
     # loop over the facial embeddings
     for encoding in encodings:
         # attempt to match each face in the input image to our known
         # encodings
         matches = face_recognition.compare_faces(data["encodings"], encoding)
-        name = "Unknown"
         # check to see if we have found a match
         if True in matches:
             # find the indexes of all matched faces then initialize a
@@ -86,32 +80,6 @@ while True:
         # update the list of names
         names.append(name)
 
-    # loop over the recognized faces
-    for ((top, right, bottom, left), name) in zip(boxes, names):
-        # rescale the face coordinates
-        top = int(top * r)
-        right = int(right * r)
-        bottom = int(bottom * r)
-        left = int(left * r)
-        # draw the predicted face name on the image
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-        y = top - 15 if top - 15 > 15 else top + 15
-        cv2.putText(
-            frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2
-        )
-
     # check to see if we are supposed to display the output frame to
     # the screen
-    if args["display"] > 0:
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
-        # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-            break
-
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
-# check to see if the video writer point needs to be released
-if writer is not None:
-    writer.release()
+    return names, verified_users
--- low_face_mode.py
+++ low_face_mode.py
@@ -2,6 +2,7 @@
 
 from cv2 import cv2
 import osascript
+from custom_recognition.recognize_faces_video import get_name
 
 
 def dim():
@@ -29,7 +30,6 @@ def brighten():
 
 
 face_cascade = cv2.CascadeClassifier("data/haarcascade_frontalface_default.xml")
-eye_cascade = cv2.CascadeClassifier("data/haarcascade_eye.xml")
 
 capture = cv2.VideoCapture(0)
 
@@ -44,29 +44,26 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     face_counter.append(len(faces))
 
-    if frames > 240:
+    users, verified_users = get_name(img)
+
+    if frames > 25:
         if dimmed == False:
             if 1 not in face_counter[frames - 20 : frames]:
                 dim()
                 dimmed = True
 
         if dimmed == True:
-            if 1 in face_counter[frames - 20 : frames]:
-                brighten()
-                dimmed = False
+            if set(users) & set(verified_users):
+                if 1 in face_counter[frames - 20 : frames]:
+                    brighten()
+                    dimmed = False
 
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi_gray = gray[y : y + h, x : x + w]
-        roi_color = img[y : y + h, x : x + w]
-
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
     cv2.imshow("Low Face Mode On", img)
     k = cv2.waitKey(30) & 0xFF
-    if k == 27:
+    if k == 27 or k == 113:
         break
 
 capture.release()
