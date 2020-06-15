175544dba7bae93759d56afe326c879f3f1e36b2
Simon <trilogicworlds@gmail.com>
2020-06-15 17:53:43 +0200

    add custom recognition

---
+++ custom_recognition/build_face_dataset.py
@@ -0,0 +1,67 @@
+'''
+Program taken and modified from https://www.pyimagesearch.com/2018/06/18/face-recognition-with-opencv-python-and-deep-learning/
+I do not make any claim over this content, which is property of Adrian Rosebrock.
+Please do not reproduce commercially without permission from the author.
+'''
+
+from imutils.video import VideoStream
+import argparse
+import imutils
+import time
+import cv2
+import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--cascade", required=True, help="Path to where cascade resides")
+ap.add_argument("-o", "--output", required=True, help="Path to output directory")
+
+args = vars(ap.parse_args())
+
+detector = cv2.CascadeClassifier(args["cascade"])
+
+parent_directory = os.getcwd()
+new_directory = args["output"]
+path = os.path.join(parent_directory, new_directory)
+os.mkdir(path)
+
+
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+
+time.sleep(2.0)
+total = 0
+
+while True:
+
+    frame = vs.read()
+    orig = frame.copy()
+    frame = imutils.resize(frame, width=400)
+
+    rects = detector.detectMultiScale(
+        cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY),
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+    )
+
+    for (x, y, w, h) in rects:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("k"):
+        p = os.path.sep.join([args["output"], "{}.png".format(str(total).zfill(5))])
+
+        cv2.imwrite(p, orig)
+        total += 1
+
+    elif key == ord("q"):
+        break
+
+print("[INFO] {} faces stored.".format(total))
+print("[INFO] cleaning up...")
+
+cv2.destroyAllWindows()
+vs.stop()
