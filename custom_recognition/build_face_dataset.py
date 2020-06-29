5b08557980391529c64d6a143280325308cac085
Simon Ilincev <61620873+Destaq@users.noreply.github.com>
2020-06-29 15:17:44 +0200

    rewrite copyright description

--- custom_recognition/build_face_dataset.py
+++ custom_recognition/build_face_dataset.py
@@ -1,8 +1,8 @@
-'''
-Parts of this program taken and modified from https://www.pyimagesearch.com/2018/06/18/face-recognition-with-opencv-python-and-deep-learning/
-I do not make any claim over this content, which is property of Adrian Rosebrock.
+"""
+Program made and modified from tutorial on https://www.pyimagesearch.com/2018/06/18/face-recognition-with-opencv-python-and-deep-learning/
+Adiran Rosebrock kindly provided this tutorial on custom face recognition, which I used and adapated for low-face-mode.
 Please do not reproduce commercially without permission from the author.
-'''
+"""
 
 from imutils.video import VideoStream
 import argparse
